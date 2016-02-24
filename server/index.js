ff71ef0f53b942606e4b180b8acbe30b084c5678
emensch <eli.mensch@gmail.com>
2016-02-23 23:08:46 -0500

    Make dummy routes, minor restructuring, use bodyParser

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static('dist'));
 
-app.use(routes);
+app.use('/api', routes);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'template.html'));
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,21 @@
-import express  from 'express';
+import express      from 'express';
+import bodyParser   from 'body-parser';
 
 const router = express.Router();
 
-router.get('/a', (req, res) => {
-    res.send('a');
+router.use(bodyParser.json());
+
+router.get('/stylesheet', (req, res) => {
+    res.sendStatus(200);
 });
+
+router.post('/vote', (req, res) => {
+    res.sendStatus(200);
+});
+
+router.post('/session', (req, res) => {
+    res.sendStatus(200);
+});
+
 
 export default router;
