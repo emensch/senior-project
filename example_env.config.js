3089c10b24136e4b02dc0879a88ca1dc78b8be57
emensch <eli.mensch@gmail.com>
2016-02-23 20:28:19 -0500

    Set up structure and install packages

---
+++ example_env.config.js
@@ -0,0 +1,5 @@
+// Set these to your values and rename file to env.config.js
+process.env['RETHINK_AUTHKEY'] = 'authkey';
+process.env['RETHINK_DBNAME'] = 'dbname';
+
+process.env['JWT_SECRET'] = 'supersecret';
