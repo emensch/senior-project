c05f184ada0eab8d8e00ff5dd8432d2af01c5e7f
emensch <eli.mensch@gmail.com>
2016-03-01 05:19:48 -0500

    Store token info in database

--- server/models/token.js
+++ server/models/token.js
@@ -1,32 +1,52 @@
 import jwt      from 'jsonwebtoken';
+import thinky   from '../utils/thinky';
 import Promise  from 'bluebird';
+const type = thinky.type;
+const r = thinky.r;
 
 const asyncVerify = Promise.promisify(jwt.verify, jwt);
 
-export function generate(email = '', numReqs = 0) {
-    const token = jwt.sign({
-        email,
-        numReqs
-    }, process.env.JWT_SECRET, {
-        expiresIn: '1h'
-    });
+const Token = thinky.createModel('Token', {
+    id: type.string(),
+    email: type.string().email(),
+    numReqs: type.number().default(0)
+});
 
-    return token;
-}
+Token.defineStatic('generate', function(email = '', numReqs = 0) {
+    const newToken = new this({email, numReqs});
+    return newToken.save()
+        .then((token) => {
+            return jwt.sign({
+                id: token.id,
+                email: token.email,
+                numReqs: token.numReqs
+            }, process.env.JWT_SECRET, {
+                expiresIn: '1h'
+            });
+        });
+});
 
-export function verify(token) {
+Token.defineStatic('checkAndIncrement', function(id, limit) {
+    return this.get(id)
+        .then(token => {
+            return token.delete();
+        })
+        .then(token => {
+            if(token.numReqs < limit) {
+                const newReqs = token.numReqs + 1;
+                const voteEnabled = (newReqs === limit);
+                return this.generate(token.email, newReqs)
+                    .then(token => {
+                        return {voteEnabled, token};
+                    })
+            } else {
+                throw new Error('limit exceeded');
+            }
+        })
+});
+
+Token.defineStatic('verify', function(token) {
     return asyncVerify(token, process.env.JWT_SECRET);
-}
+});
 
-export function incrementAndCheck(token, limit) {
-    return new Promise( (resolve, reject) => {
-        if (token.numReqs < limit) {
-            const newReqs = token.numReqs + 1;
-            const voteEnabled = (newReqs === limit);
-            const data = {voteEnabled, token: generate(token.email, newReqs)}
-            resolve(data);
-        } else {
-            reject(new Error('limit exceeded'));
-        }
-    });
-}
+export default Token;
