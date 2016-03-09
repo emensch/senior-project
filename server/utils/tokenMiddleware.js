05ba2e7f6cd68ebc89eec313b9b788aa7e4d5961
emensch <eli.mensch@gmail.com>
2016-03-09 02:45:48 -0500

    Strip logging statements

--- server/utils/tokenMiddleware.js
+++ server/utils/tokenMiddleware.js
@@ -2,14 +2,12 @@ import Token    from '../models/token';
 
 export default function(req, res, next) {
     const token = req.headers['x-access-token'];
-    console.log(token);
     if(token) {
         return Token.verify(token)
             .then(decoded => {
-                console.log('decoded: ', decoded);
                 return Token.get(decoded.id)
-                    .then(() => {
-                        req.token = decoded;
+                    .then((retrieved) => {
+                        req.token = retrieved;
                         next();
                     });
             })
