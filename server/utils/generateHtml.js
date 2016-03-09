e062bd8b6eb0ca71ff19286f079a2c8ca12821f4
emensch <eli.mensch@gmail.com>
2016-03-09 05:22:28 -0500

    Append <style> tag to top of container div

--- server/utils/generateHtml.js
+++ server/utils/generateHtml.js
@@ -5,14 +5,18 @@ import path     from 'path';
 import fs       from 'fs';
 
 const renderSass = Promise.promisify(sass.render, sass);
-const html = fs.readFileSync(path.join(__dirname, '../genetic_assets/template.html'), {encoding: 'utf-8'});
-const scss = fs.readFileSync(path.join(__dirname, '../genetic_assets/styles.scss'), {encoding: 'utf-8'});
+const html = fs.readFileSync(path.join(__dirname, '../genetic_utils/template.html'), {encoding: 'utf-8'});
+const scss = fs.readFileSync(path.join(__dirname, '../genetic_utils/styles.scss'), {encoding: 'utf-8'});
 
 export default function(styleVars) {
     const scssWithVars = genVariables(styleVars) + scss;
     return renderSass({data: scssWithVars, includePaths: ['node_modules/']})
         .then(data => {
-            return juice(html, {extraCss: data.css.toString(), preserveFontFaces: true, insertPreservedExtraCss: true});
+            return juice(html, {
+                extraCss: data.css.toString(),
+                preserveFontFaces: true,
+                insertPreservedExtraCss: '#dynamic-content'
+            });
         })
 }
 
