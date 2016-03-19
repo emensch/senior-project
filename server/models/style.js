9df389bcda912d25f2232cc35a85fe1fa228ce58
emensch <eli.mensch@gmail.com>
2016-03-19 03:16:18 -0400

    Add method for creating new generation

--- server/models/style.js
+++ server/models/style.js
@@ -1,5 +1,6 @@
-import thinky   from '../utils/thinky';
-import Promise  from 'bluebird';
+import thinky       from '../utils/thinky';
+import Promise      from 'bluebird';
+import Chromosome   from '../genetic_utils/chromosome';
 const type = thinky.type;
 const r = thinky.r;
 
@@ -7,7 +8,7 @@ const Style = thinky.createModel('Style', {
     id: type.string(),
     generation: type.number(),
     styles: type.object(),
-    fitness: type.number().default(0),
+    fitness: type.number().default(1),
     parents: [type.string()],
     visited: type.number().default(0),
     createdOn: type.date().default(r.now()) 
@@ -37,21 +38,75 @@ Style.defineStatic('processVote', function(id) {
 });
 
 Style.defineStatic('countVotesAndGenerate', function() {
-   return r.table('Style')
-       .max('generation')
+    return r.table('Style')
+        .max('generation')
         .then(res => {
-            let maxGen = res.generation;
+            let currentGen = res.generation;
             return r.table('Style')
-                .filter({generation: maxGen})
+                .filter({generation: currentGen})
                 .sum('fitness')
         })
         .then(num => {
             if(num > 30) {
-                return; //make new generation
+                return this.createNewGeneration()
             } else {
                 return;
             }
         });
 });
+
+Style.defineStatic('createNewGeneration', function() {
+    return r.table('Style')
+        .max('generation')
+        .then(res => {
+            let currentGen = res.generation;
+            return this.filter({generation: currentGen}).execute()
+                .then(pop => {
+                    let newPop = [];
+
+                    while(newPop.length < 10) {
+                        const indices = rouletteSelection(pop, 2);
+                        const cr1ID = pop[indices[0]].id;
+                        const cr1 = new Chromosome(pop[indices[0]].styles);
+                        const cr2ID = pop[indices[1]].id;
+                        const cr2 = new Chromosome(pop[indices[1]].styles);
+
+                        const newCr = Chromosome.uniformCrossover(cr1, cr2);
+                        newCr.mutate();
+
+                        newPop.push({
+                            generation: currentGen + 1,
+                            styles: newCr.getObjectRepresentation(),
+                            parents: [cr1ID, cr2ID]
+                        });
+                    }
+
+                    this.save(newPop);
+                });
+        });
+});
+
+function rouletteSelection(pop, num) {
+
+    const fitnesses = pop.map(obj => obj.fitness);
+    const totalFitness = fitnesses.reduce((total, current) => {
+        return total + current;
+    });
+
+    let indices = [];
+
+    while(num--) {
+        let randVal = Math.floor(Math.random() * totalFitness)
+        for(let idx = 0; idx < fitnesses.length; idx++) {
+            randVal -= fitnesses[idx];
+            if(randVal <= 0) {
+                indices.push(idx);
+                break;
+            }
+        }
+    }
+
+    return indices;
+}
 
 export default Style;
