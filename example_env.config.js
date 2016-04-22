573c8d8461c5f20edf0f6ca5edcea68a69742789
emensch <eli.mensch@gmail.com>
2016-04-22 07:09:28 -0400

    Add configurable db connection pool size

--- example_env.config.js
+++ example_env.config.js
@@ -1,6 +1,7 @@
 // Set these to your values and rename file to env.config.js
 process.env['RETHINK_AUTHKEY'] = 'authkey';
 process.env['RETHINK_DBNAME'] = 'dbname';
+process.env['RETHINK_POOL_BUFFER'] = 15;
 
 process.env['JWT_SECRET'] = 'supersecret';
 
--- server/utils/thinky.js
+++ server/utils/thinky.js
@@ -1,7 +1,8 @@
 import thinky from 'thinky';
 
 const config = {
-    db: process.env.RETHINK_DBNAME
+    db: process.env.RETHINK_DBNAME,
+    buffer: process.env.RETHINK_POOL_BUFFER
 }
 
 export default thinky(config);
